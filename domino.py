b57bed7373cfca8b8050e1f57672cd88d1ca5265
Ray Schone <ray66928@gmail.com>
2022-10-03 20:04:59 -0400

    changed the way the dominos are created

--- domino.py
+++ domino.py
@@ -24,8 +24,16 @@ class Domino:
 class Set:
     domino = []
 
-    def __init__(self):
+    def __init__(self, size):
         self.domino = []
+
+        for top in range(0, size + 1):
+            for bottom in range(top, size + 1):
+                top_side = Side(Pips(top))
+                bottom_side = Side(Pips(bottom))
+                new_domino = Domino(top_side, bottom_side)
+                self.add(new_domino)
+                print("created: ", top, bottom)
 
     def add(self, domino):
         self.domino.append(domino)
